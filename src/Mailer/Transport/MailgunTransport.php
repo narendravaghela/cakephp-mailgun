d6801bbba28fdcc21ab2f85aa641b68570dbabe9
Alysson Azevedo <alysson@rxsaude.com.br>
2018-06-18 13:24:23 -0300

    Change isset to !empty to ensure contentId is a valid string.

--- src/Mailer/Transport/MailgunTransport.php
+++ src/Mailer/Transport/MailgunTransport.php
@@ -250,7 +250,7 @@ class MailgunTransport extends AbstractTransport
         $attachments = [];
 
         foreach ($this->_cakeEmail->attachments() as $name => $file) {
-            if (isset($file['contentId'])) {
+            if (!empty($file['contentId'])) {
                 $attachments['inline'][] = ['filePath' => '@' . $file['file'], 'remoteName' => $file['contentId']];
             }
             else {
