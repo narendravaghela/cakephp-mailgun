767bdce7814429b24025b66b51fbcaf58f8af714
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-10-06 14:23:04 +0530

    Update phpdoc blocks

--- src/Mailer/Transport/MailgunTransport.php
+++ src/Mailer/Transport/MailgunTransport.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Mailgun Plugin for CakePHP 3
+ * Copyright (c) Narendra Vaghela (http://www.narendravaghela.com)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.md
  * Redistributions of files must retain the above copyright notice.
  *
+ * @copyright Copyright (c) Narendra Vaghela (http://www.narendravaghela.com)
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  * @link      https://github.com/narendravaghela/cakephp-mailgun
  * @since     1.0.0
--- src/Plugin.php
+++ src/Plugin.php
@@ -1,14 +1,16 @@
 <?php
 /**
  * Mailgun Plugin for CakePHP 3
+ * Copyright (c) Narendra Vaghela (http://www.narendravaghela.com)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.md
  * Redistributions of files must retain the above copyright notice.
  *
+ * @copyright Copyright (c) Narendra Vaghela (http://www.narendravaghela.com)
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  * @link      https://github.com/narendravaghela/cakephp-mailgun
- * @since     1.0.0
+ * @since     3.0.0
  */
 
 namespace Mailgun;
