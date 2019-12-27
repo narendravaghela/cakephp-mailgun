53d0d875cdaeb0c2ad41279eeca2796d28d72359
Chris Hallgren <challgren@gmail.com>
2019-12-27 04:11:00 -0600

    Reorganizing test methods
    Signed-off-by: Chris Hallgren <challgren@gmail.com>

--- tests/TestCase/Mailer/Transport/MailgunTransportTest.php
+++ tests/TestCase/Mailer/Transport/MailgunTransportTest.php
@@ -39,108 +39,6 @@ class MailgunTransportTest extends TestCase
         Configure::write('DebugKit.panels', ['DebugKit.Mail' => false]);
     }
 
-    public function testSendEmailTransport(): void
-    {
-        $data = [
-            'id' => '<5a6387fa5a0b46c79489c9b998b101c4@Mac-mini.local>',
-            'message' => 'Queued. Thank you.',
-        ];
-        $this->MailgunTransport = new MailgunTransport();
-        $mock = $this->getMockBuilder('Cake\Http\Client')->onlyMethods(['post'])->getMock();
-        $response = (new Response([], json_encode($data)))->withStatus(200);
-        $mock->expects($this->once())->method('post')->willReturn($response);
-        $this->MailgunTransport->Client = $mock;
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom('test@example.com');
-        $result = $this->MailgunTransport->send($message);
-        $this->assertSame($data['id'], $result['apiResponse']['id']);
-        $this->assertSame($data['message'], $result['apiResponse']['message']);
-    }
-
-    public function testSendEmailTransportFailure(): void
-    {
-        $this->expectExceptionMessage('Forbidden');
-        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
-        $this->MailgunTransport = new MailgunTransport();
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom('test@example.com');
-        $this->MailgunTransport->send($message);
-
-        $this->assertNull($this->MailgunTransport->getRequestData());
-    }
-
-    public function testSendEmail(): void
-    {
-        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom('test@example.com');
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailFrom(): void
-    {
-        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
-        $this->MailgunTransport->expects($this->once())->method('_reset');
-        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom(['test@example.com' => 'Test Email']);
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailCc(): void
-    {
-        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
-        $this->MailgunTransport->expects($this->once())->method('_reset');
-        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom(['test@example.com' => 'Test Email'])
-            ->setCc(['cc@example.com' => 'CC Email']);
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailBcc(): void
-    {
-        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
-        $this->MailgunTransport->expects($this->once())->method('_reset');
-        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $message->setFrom(['test@example.com' => 'Test Email'])
-            ->setBcc(['cc@example.com' => 'CC Email']);
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailNoDomain(): void
-    {
-        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
-        $this->expectExceptionMessage('Domain for Mailgun could not found.');
-        $this->MailgunTransport->setConfig(['apiKey' => '123']);
-        $message = new Message();
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailNoApiKey(): void
-    {
-        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
-        $this->expectExceptionMessage('Api Key for Mailgun could not found.');
-        $message = new Message();
-        $this->MailgunTransport->send($message);
-    }
-
-    public function testSendEmailNoFrom(): void
-    {
-        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
-        $this->expectExceptionMessage('Missing from email address.');
-        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
-        $message = new Message();
-        $this->MailgunTransport->send($message);
-    }
-
     public function testAdditionalEmailAddresses()
     {
         $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
@@ -161,7 +59,6 @@ class MailgunTransportTest extends TestCase
             ->setSubject('Email from CakePHP Mailgun plugin');
         $this->MailgunTransport->send($message);
 
-        /** @var FormData $reqData */
         $reqData = $this->MailgunTransport->getRequestData();
         $boundary = $reqData->boundary();
         $reqDataString = (string)$reqData;
@@ -231,7 +128,7 @@ class MailgunTransportTest extends TestCase
         $this->assertStringEndsWith("--$boundary--", rtrim($reqDataString));
     }
 
-    public function testSetRecipientVars()
+    public function testSendBatchEmail()
     {
         $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
         $this->MailgunTransport->expects($this->once())->method('_reset');
@@ -239,16 +136,12 @@ class MailgunTransportTest extends TestCase
         $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
         $message = new Message();
 
-        $recipientData = [
-            'foo@example.com' => ['name' => 'Foo Bar'],
-            'john@example.com' => ['name' => 'John Doe'],
-        ];
-        $message->addHeaders(['X-Mailgun-Recipient-Variables' => json_encode($recipientData)]);
-
-        $message->setFrom('from@example.com')
-            ->setTo('foo@example.com')
-            ->addTo('john@example.com')
+        $message->setFrom(['from@example.com' => 'CakePHP Mailgun Email'])
+            ->setTo('to@example.com')
+            ->addTo(['bar@example.com', 'john@example.com' => 'John'])
+            ->setEmailFormat('both')
             ->setSubject('Email from CakePHP Mailgun plugin');
+
         $this->MailgunTransport->send($message);
 
         $reqData = $this->MailgunTransport->getRequestData();
@@ -257,36 +150,115 @@ class MailgunTransportTest extends TestCase
 
         $this->assertNotEmpty($reqDataString);
         $this->assertStringStartsWith("--$boundary", $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="recipient-variables"', $reqDataString);
-        $this->assertTextContains(json_encode($recipientData), $reqDataString);
+        $this->assertTextContains('Content-Disposition: form-data; name="from"', $reqDataString);
+        $this->assertTextContains('Content-Disposition: form-data; name="to"', $reqDataString);
+        $this->assertTextContains('CakePHP Mailgun Email <from@example.com>', $reqDataString);
+        $this->assertTextContains('to@example.com <to@example.com>', $reqDataString);
+        $this->assertTextContains('bar@example.com <bar@example.com>', $reqDataString);
+        $this->assertTextContains('John <john@example.com>', $reqDataString);
+        $this->assertStringEndsWith("--$boundary--", rtrim($reqDataString));
     }
 
-    public function testSetOption()
+    public function testSendEmail(): void
+    {
+        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $message->setFrom('test@example.com');
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailBcc(): void
     {
         $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
         $this->MailgunTransport->expects($this->once())->method('_reset');
         $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
         $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
         $message = new Message();
+        $message->setFrom(['test@example.com' => 'Test Email'])
+            ->setBcc(['cc@example.com' => 'CC Email']);
+        $this->MailgunTransport->send($message);
+    }
 
-        $message->setFrom('from@example.com')
-            ->setTo('to@example.com')
-            ->setSubject('Email from CakePHP Mailgun plugin')
-            ->addHeaders(['X-Mailgun-Drop-Message' => 'yes', 'X-Mailgun-Tag' => json_encode(['newsletter', 'welcome email'])]);
+    public function testSendEmailCc(): void
+    {
+        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
+        $this->MailgunTransport->expects($this->once())->method('_reset');
+        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $message->setFrom(['test@example.com' => 'Test Email'])
+            ->setCc(['cc@example.com' => 'CC Email']);
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailFrom(): void
+    {
+        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
+        $this->MailgunTransport->expects($this->once())->method('_reset');
+        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $message->setFrom(['test@example.com' => 'Test Email']);
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailNoApiKey(): void
+    {
+        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
+        $this->expectExceptionMessage('Api Key for Mailgun could not found.');
+        $message = new Message();
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailNoDomain(): void
+    {
+        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
+        $this->expectExceptionMessage('Domain for Mailgun could not found.');
+        $this->MailgunTransport->setConfig(['apiKey' => '123']);
+        $message = new Message();
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailNoFrom(): void
+    {
+        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
+        $this->expectExceptionMessage('Missing from email address.');
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $this->MailgunTransport->send($message);
+    }
+
+    public function testSendEmailTransport(): void
+    {
+        $data = [
+            'id' => '<5a6387fa5a0b46c79489c9b998b101c4@Mac-mini.local>',
+            'message' => 'Queued. Thank you.',
+        ];
+        $this->MailgunTransport = new MailgunTransport();
+        $mock = $this->getMockBuilder('Cake\Http\Client')->onlyMethods(['post'])->getMock();
+        $response = (new Response([], json_encode($data)))->withStatus(200);
+        $mock->expects($this->once())->method('post')->willReturn($response);
+        $this->MailgunTransport->Client = $mock;
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $message->setFrom('test@example.com');
+        $result = $this->MailgunTransport->send($message);
+        $this->assertSame($data['id'], $result['apiResponse']['id']);
+        $this->assertSame($data['message'], $result['apiResponse']['message']);
+    }
+
+    public function testSendEmailTransportFailure(): void
+    {
+        $this->expectExceptionMessage('Forbidden');
+        $this->expectException('Mailgun\Mailer\Exception\MailgunApiException');
+        $this->MailgunTransport = new MailgunTransport();
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+        $message->setFrom('test@example.com');
         $this->MailgunTransport->send($message);
 
-        $reqData = $this->MailgunTransport->getRequestData();
-        $boundary = $reqData->boundary();
-        $reqDataString = (string)$reqData;
-
-        $this->assertNotEmpty($reqDataString);
-        $this->assertStringStartsWith("--$boundary", $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="o:testmode"', $reqDataString);
-        $this->assertTextContains('yes', $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="o:tag[0]"', $reqDataString);
-        $this->assertTextContains('newsletter', $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="o:tag[1]"', $reqDataString);
-        $this->assertTextContains('welcome email', $reqDataString);
+        $this->assertNull($this->MailgunTransport->getRequestData());
     }
 
     public function testSetCustomMessageData()
@@ -339,7 +311,7 @@ class MailgunTransportTest extends TestCase
         $this->assertTextContains(json_encode($customMessageData), $reqDataString);
     }
 
-    public function testSendBatchEmail()
+    public function testSetOption()
     {
         $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
         $this->MailgunTransport->expects($this->once())->method('_reset');
@@ -347,12 +319,10 @@ class MailgunTransportTest extends TestCase
         $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
         $message = new Message();
 
-        $message->setFrom(['from@example.com' => 'CakePHP Mailgun Email'])
+        $message->setFrom('from@example.com')
             ->setTo('to@example.com')
-            ->addTo(['bar@example.com', 'john@example.com' => 'John'])
-            ->setEmailFormat('both')
-            ->setSubject('Email from CakePHP Mailgun plugin');
-
+            ->setSubject('Email from CakePHP Mailgun plugin')
+            ->addHeaders(['X-Mailgun-Drop-Message' => 'yes', 'X-Mailgun-Tag' => json_encode(['newsletter', 'welcome email'])]);
         $this->MailgunTransport->send($message);
 
         $reqData = $this->MailgunTransport->getRequestData();
@@ -361,12 +331,41 @@ class MailgunTransportTest extends TestCase
 
         $this->assertNotEmpty($reqDataString);
         $this->assertStringStartsWith("--$boundary", $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="from"', $reqDataString);
-        $this->assertTextContains('Content-Disposition: form-data; name="to"', $reqDataString);
-        $this->assertTextContains('CakePHP Mailgun Email <from@example.com>', $reqDataString);
-        $this->assertTextContains('to@example.com <to@example.com>', $reqDataString);
-        $this->assertTextContains('bar@example.com <bar@example.com>', $reqDataString);
-        $this->assertTextContains('John <john@example.com>', $reqDataString);
-        $this->assertStringEndsWith("--$boundary--", rtrim($reqDataString));
+        $this->assertTextContains('Content-Disposition: form-data; name="o:testmode"', $reqDataString);
+        $this->assertTextContains('yes', $reqDataString);
+        $this->assertTextContains('Content-Disposition: form-data; name="o:tag[0]"', $reqDataString);
+        $this->assertTextContains('newsletter', $reqDataString);
+        $this->assertTextContains('Content-Disposition: form-data; name="o:tag[1]"', $reqDataString);
+        $this->assertTextContains('welcome email', $reqDataString);
+    }
+
+    public function testSetRecipientVars()
+    {
+        $this->MailgunTransport = $this->getMockBuilder('Mailgun\Mailer\Transport\MailgunTransport')->onlyMethods(['_sendEmail', '_reset'])->getMock();
+        $this->MailgunTransport->expects($this->once())->method('_reset');
+        $this->MailgunTransport->expects($this->once())->method('_sendEmail')->willReturn([]);
+        $this->MailgunTransport->setConfig(['apiKey' => '123', 'domain' => 'example.com']);
+        $message = new Message();
+
+        $recipientData = [
+            'foo@example.com' => ['name' => 'Foo Bar'],
+            'john@example.com' => ['name' => 'John Doe'],
+        ];
+        $message->addHeaders(['X-Mailgun-Recipient-Variables' => json_encode($recipientData)]);
+
+        $message->setFrom('from@example.com')
+            ->setTo('foo@example.com')
+            ->addTo('john@example.com')
+            ->setSubject('Email from CakePHP Mailgun plugin');
+        $this->MailgunTransport->send($message);
+
+        $reqData = $this->MailgunTransport->getRequestData();
+        $boundary = $reqData->boundary();
+        $reqDataString = (string)$reqData;
+
+        $this->assertNotEmpty($reqDataString);
+        $this->assertStringStartsWith("--$boundary", $reqDataString);
+        $this->assertTextContains('Content-Disposition: form-data; name="recipient-variables"', $reqDataString);
+        $this->assertTextContains(json_encode($recipientData), $reqDataString);
     }
 }
